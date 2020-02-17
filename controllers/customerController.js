7fd8b20b83fe991b7f8ab510bf0e87ee18c6e5ca
psi9730 <psi9730@naver.com>
2020-02-17 10:20:53 +0900

    add totalcustomernum

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -3,6 +3,13 @@ const response = require('../utils/jsonResponse');
 
 exports.getAllUser = async (req, res, next) => {
   let query = connection('customer');
+  if (Object.keys(req.query).length > 0) {
+    Object.keys(req.query).forEach((property) => {
+      if (req.query[property] && property !== 'limit' && property !== 'offset') query = query.where(property, 'like', `%${req.query[property]}%`);
+    });
+  }
+  const totalCustomerNum = await query.clone().count().catch((err) => { next(err); });
+
   if (Object.prototype.hasOwnProperty.call(req.query, 'limit')) {
     query = query.limit(req.query.limit);
     delete req.query.limit;
@@ -11,16 +18,11 @@ exports.getAllUser = async (req, res, next) => {
     query = query.offset(req.query.offset);
     delete req.query.offset;
   }
-  if (Object.keys(req.query).length > 0) {
-    Object.keys(req.query).forEach((property) => {
-      if (req.query[property]) query = query.where(property, 'like', `%${req.query[property]}%`);
-    });
-  }
-  query.select('id', 'name', 'phone AS phoneNumber')
-    .then(
-      (rows) => res.json(response(200, rows)),
-    )
-    .catch((err) => { next(err); });
+  const customers = await query.select('id', 'name', 'phone AS phoneNumber').catch((err) => { next(err); });
+  res.json(response(200, {
+    customers,
+    totalCustomerNum: totalCustomerNum[0]['count(*)'],
+  }));
 };
 exports.postUser = async (req, res, next) => {
   connection('customer')
